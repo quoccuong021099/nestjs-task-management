7c1c9b60d4600b96685f427464983b0edf174a5f
quoccuong021099 <quoccuong021099@gmail.com>
2021-09-08 13:42:58 +0700

    A

---
+++ README.md
@@ -0,0 +1,6 @@
+1. tạo một module
+   `nest g module tasks`
+2. tạo 1 controller
+   `nest g controller tasks --no-spec`
+3. tạo 1 service
+   `nest g service tasks --no-spec`
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+  // hello() {
+  //   this.tasksService.hello();
+  // }
+}
